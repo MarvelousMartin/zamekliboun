6af5cd019890af0be735398d885b89d46095afe4
Martin Choutka <martafiixek@gmail.com>
2021-08-08 22:16:04 +0200

    Add default Next pages

---
+++ pages/_app.jsx
@@ -0,0 +1,16 @@
+import "tailwindcss/tailwind.css";
+import { appWithTranslation } from "next-i18next";
+import { Layout } from "components/layouts/Layout";
+import { Provider } from "next-auth/client";
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <Provider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
+};
+
+export default appWithTranslation(MyApp);
