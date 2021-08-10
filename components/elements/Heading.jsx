e47a2f75518217fc8565f9314aa1573bcfc72d56
Martin Choutka <martafiixek@gmail.com>
2021-08-10 23:30:14 +0200

    Update Heading component

--- components/elements/Heading.jsx
+++ components/elements/Heading.jsx
@@ -2,18 +2,20 @@ export default function Heading({ as, style, children }) {
   let heading;
   switch (as) {
     case "h1":
-      heading = <h1 className="font-black text-3xl mb-1">{children}</h1>;
-      break;
-    case "h2":
       heading = (
-        <h2 className="font-medium text-lg mb-4 text-gray-500">{children}</h2>
+        <h1 className={`font-black text-3xl mb-4 ${style}`}>{children}</h1>
       );
       break;
+    case "h2":
+      heading = <h2 className="font-bold text-2xl mb-4">{children}</h2>;
+      break;
     case "h3":
-      heading = <h3 className="font-black text-3xl mb-1">{children}</h3>;
+      heading = <h3 className="font-bold text-xl mb-1">{children}</h3>;
       break;
     case "h4":
-      heading = <h4 className="font-black text-3xl mb-1">{children}</h4>;
+      heading = (
+        <h4 className="font-medium text-lg mb-4 text-gray-500">{children}</h4>
+      );
       break;
     case "h5":
       heading = <h5 className="font-black text-3xl mb-1">{children}</h5>;
