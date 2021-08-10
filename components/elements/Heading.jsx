1d7703833fbf2ea99c5e058adb8dd7eec842e45e
Martin Choutka <martafiixek@gmail.com>
2021-08-10 19:59:31 +0200

    Add Heading component

---
+++ components/elements/Heading.jsx
@@ -0,0 +1,28 @@
+export default function Heading({ as, style, children }) {
+  let heading;
+  switch (as) {
+    case "h1":
+      heading = <h1 className="font-black text-3xl mb-1">{children}</h1>;
+      break;
+    case "h2":
+      heading = (
+        <h2 className="font-medium text-lg mb-4 text-gray-500">{children}</h2>
+      );
+      break;
+    case "h3":
+      heading = <h3 className="font-black text-3xl mb-1">{children}</h3>;
+      break;
+    case "h4":
+      heading = <h4 className="font-black text-3xl mb-1">{children}</h4>;
+      break;
+    case "h5":
+      heading = <h5 className="font-black text-3xl mb-1">{children}</h5>;
+      break;
+    case "h6":
+      heading = <h6 className="font-black text-3xl mb-1">{children}</h6>;
+      break;
+    default:
+    // code block
+  }
+  return heading;
+}
