6c35ad91303a9a4ac66f27888a715995ec0adc5f
Martin Choutka <martafiixek@gmail.com>
2021-08-10 20:01:01 +0200

    Add & change colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false,
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      damask: {
+        700: "#c36f4b",
+        600: "#c97d5d",
+      },
+    },
     fontFamily: {
       sans: ["STIX Two Text", "system-ui"],
     },
