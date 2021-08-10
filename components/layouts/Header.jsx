a894015d8c27cd56efc69f9312de8377a3a51499
Martin Choutka <martafiixek@gmail.com>
2021-08-10 20:26:01 +0200

    Update menu array map object

--- components/layouts/Header.jsx
+++ components/layouts/Header.jsx
@@ -216,63 +216,11 @@ export default function Header() {
                   </Popover>
                 ))}
                 {menu.map((item) => (
-                  <Popover className="relative" key={item.name}>
-                    {({ open }) => (
-                      <>
-                        <Popover.Button
-                          className={classNames(
-                            open
-                              ? "text-gray-900 dark:text-gray-200"
-                              : "text-gray-500 dark:text-gray-200",
-                            "group bg-white dark:bg-gray-900 rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 dark:hover:text-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-8 focus:ring-damask-600 dark:ring-offset-gray-900"
-                          )}
-                        >
-                          <span>{item.name}</span>
-                        </Popover.Button>
-
-                        <Transition
-                          show={open}
-                          as={Fragment}
-                          enter="transition ease-out duration-200"
-                          enterFrom="opacity-0 translate-y-1"
-                          enterTo="opacity-100 translate-y-0"
-                          leave="transition ease-in duration-150"
-                          leaveFrom="opacity-100 translate-y-0"
-                          leaveTo="opacity-0 translate-y-1"
-                        >
-                          <Popover.Panel
-                            static
-                            className="absolute z-50 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2"
-                          >
-                            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                              <div className="relative grid gap-6 bg-white dark:bg-gray-900 px-5 py-6 sm:gap-8 sm:p-8">
-                                {additionalMenu.map((item) => (
-                                  <Link href={item.href} key={item.name}>
-                                    <a className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50 dark:hover:bg-gray-800">
-                                      <FontAwesomeIcon
-                                        fixedWidth
-                                        icon={item.icon}
-                                        className="flex-shrink-0 text-damask-700"
-                                        aria-hidden="true"
-                                      />
-                                      <div className="ml-4">
-                                        <p className="text-base font-medium text-gray-900 dark:text-gray-200">
-                                          {item.name}
-                                        </p>
-                                        <p className="mt-1 text-sm text-gray-500 dark:text-gray-300">
-                                          {item.description}
-                                        </p>
-                                      </div>
-                                    </a>
-                                  </Link>
-                                ))}
-                              </div>
-                            </div>
-                          </Popover.Panel>
-                        </Transition>
-                      </>
-                    )}
-                  </Popover>
+                  <Link href={item.href}>
+                    <a className="text-gray-500 dark:text-gray-200 text-base font-medium">
+                      {item.name}
+                    </a>
+                  </Link>
                 ))}
                 <Popover className="relative">
                   {({ open }) => (
