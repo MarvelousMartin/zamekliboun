eefd872395ca68df78071fb1ee31bb56cd79b723
Martin Choutka <martafiixek@gmail.com>
2021-08-11 11:22:39 +0200

    Update gallery page
    Meta tag title & subheading component

--- pages/galerie.jsx
+++ pages/galerie.jsx
@@ -1,6 +1,8 @@
 import Gallery from "components/elements/GalleryLightbox/Gallery";
 import Heading from "components/elements/Heading";
+import Subheading from "components/elements/Subheading";
 import Main from "components/elements/Main";
+import Head from "next/head";
 
 export default function Galerie() {
   const images = [
@@ -42,30 +44,35 @@ export default function Galerie() {
     },
   ];
   return (
-    <Main>
-      <Heading as="h1">Galerie</Heading>
-      <Heading as="h2">Podívejte se na fotky našeho zámku</Heading>
-      <div className="grid lg:grid-cols-2 gap-x-8">
-        <Gallery images={images} />
-        <div>
-          <p className="mb-3">
-            Hostům nabízíme ubytování na zámku i v penzionu v podzámčí či
-            podkroví. Dovolená na zámku Libouň je vhodná pro všechny věkové
-            kategorie. Pro romantické pobyty doporučujeme ubytování na zámku v
-            zámeckých apartmánech, pro rodiny s dětmi je vhodný penzion v
-            podzámčí či v podkroví.
-          </p>
-          <p>
-            Zámek Libouň je ideálním místem pro rodinné oslavy, firemní akce a
-            svatby. K dispozici je zámecká restaurace, dva salónky, konírna s
-            celkovou kapacitou cca 200 osob. Relaxujte v zámecké zahradě s
-            kašnou nebo u rybníka či tůni. Zasportujte si na našem víceúčelovém
-            hřišti a pak se osvěžte v našem venkovním krytém vyhřívaném bazénu.
-            Na přání nabízíme i masáž. Naší specializací je pořádání svateb na
-            klíč.
-          </p>
+    <>
+      <Head>
+        <title>Zámek Libouň - Galerie</title>
+      </Head>
+      <Main>
+        <Heading as="h1">Galerie</Heading>
+        <Subheading>Podívejte se na fotky našeho zámku</Subheading>
+        <div className="grid lg:grid-cols-2 gap-x-8">
+          <Gallery images={images} />
+          <div>
+            <p className="mb-3">
+              Hostům nabízíme ubytování na zámku i v penzionu v podzámčí či
+              podkroví. Dovolená na zámku Libouň je vhodná pro všechny věkové
+              kategorie. Pro romantické pobyty doporučujeme ubytování na zámku v
+              zámeckých apartmánech, pro rodiny s dětmi je vhodný penzion v
+              podzámčí či v podkroví.
+            </p>
+            <p>
+              Zámek Libouň je ideálním místem pro rodinné oslavy, firemní akce a
+              svatby. K dispozici je zámecká restaurace, dva salónky, konírna s
+              celkovou kapacitou cca 200 osob. Relaxujte v zámecké zahradě s
+              kašnou nebo u rybníka či tůni. Zasportujte si na našem
+              víceúčelovém hřišti a pak se osvěžte v našem venkovním krytém
+              vyhřívaném bazénu. Na přání nabízíme i masáž. Naší specializací je
+              pořádání svateb na klíč.
+            </p>
+          </div>
         </div>
-      </div>
-    </Main>
+      </Main>
+    </>
   );
 }
